48e73469dc567e45166d0e5e737913822c99e926
PhilGebauer <gebauer.phil@gmail.com>
2017-03-17 12:11:20 -0500

    bootcamp layout finished

--- public/scripts/client-app.js
+++ public/scripts/client-app.js
@@ -1,4 +1,4 @@
-var app = angular.module('app', ['ngRoute', 'firebase']);
+var app = angular.module('app', ['ngRoute', 'firebase', 'truncate']);
 
 
 app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
